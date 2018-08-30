dabbd6e1fb03398f088b892576a0d5079179193e
David Salgado <david@digitalronin.com>
2018-08-30 14:09:54 +0100

    Remove unnecessary script

--- create_key.py
+++
@@ -1,9 +0,0 @@
-from uplink import *
-import binascii
-
-rpc = UplinkJsonRpc(host="localhost", port=8545, tls=False)
-
-pubkey, skey = ecdsa_new()
-
-print("Public key: \n" + binascii.hexlify(pubkey.to_string()))
-print("Private key: \n" + binascii.hexlify(skey.to_string()))
