47e79b93d5c694bb82b8fcf77421354e594d2fb0
David Salgado <david@digitalronin.com>
2018-08-30 14:21:32 +0100

    Remove duplicated line

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from uplink import *
 rpc = UplinkJsonRpc(host="localhost", port=8545, tls=False)
 
 print rpc.uplink_blocks()
-print rpc.uplink_blocks()
 print rpc.uplink_peers()
 print rpc.uplink_accounts()
 print rpc.uplink_assets()
